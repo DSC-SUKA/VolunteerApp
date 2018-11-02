bb31a0960dfe196541bef05ffc7d2cad3b691246
azkihidayatulloh <azkihidayatulloh@gmail.com>
2018-11-02 14:45:26 +0700

    Add ItemClickSupport

---
+++ app/src/main/java/com/dicoding/millatip/volunteerapp/LatestContributionExtendedActivity.java
@@ -0,0 +1,13 @@
+package com.dicoding.millatip.volunteerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LatestContributionExtendedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latest_contribution_extended);
+    }
+}
